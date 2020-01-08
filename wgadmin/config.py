abc2dd34661b60f455e2556c9617d5d9c6a20681
Arti Zirk <arti.zirk@gmail.com>
2020-01-08 23:36:19 +0200

    Add option for one time key and config generation

--- wgadmin/config.py
+++ wgadmin/config.py
@@ -4,6 +4,7 @@ from sqlalchemy.exc import IntegrityError
 from flask import (
     Blueprint, Response, flash, g, redirect, render_template, request, url_for
 )
+from http import HTTPStatus
 
 from . import db
 from .models import Interface, IpAddress, Peer
@@ -13,9 +14,12 @@ from . import utils
 
 bp = Blueprint("config", __name__)
 
-def generate_wg_config(id, variant="quick"):
+
+def generate_wg_config(id, variant="quick", private_key_placeholder=True):
     iface = Interface.query.get_or_404(id)
-    body = ["""[Interface]\n# PrivateKey = REPLACE ME!!"""]
+    body = ["[Interface]"]
+    if private_key_placeholder:
+        body.append("\n# PrivateKey = REPLACE ME!!")
     body.append("\n# PublicKey = " + iface.public_key)
     if iface.listen_port:
         body.append("\nListenPort = " + str(iface.listen_port))
@@ -44,10 +48,36 @@ def generate_wg_config(id, variant="quick"):
 
     return Response(body, mimetype='text/plain')
 
+
 @bp.route("/export/<int:id>/wg-quick")
 def export_wg_quick(id):
     return generate_wg_config(id, "quick")
 
+
 @bp.route("/export/<int:id>/wg-sync")
 def export_wg_synx(id):
-    return generate_wg_config(id, "sync")
+    return generate_wg_config(id, "sync",
+                              private_key_placeholder=False)
+
+
+@bp.route("/export/<int:id>/onetime", methods=("GET", "POST"))
+def export_onetime(id):
+    iface = Interface.query.get_or_404(id)
+    if request.method == "POST":
+        iface.public_key = request.form.get("publicKey")
+        db.session.add(iface)
+        try:
+            db.session.commit()
+            return generate_wg_config(id, "quick",
+                                      private_key_placeholder=False)
+        except IntegrityError as e:
+            db.session.rollback()
+            if "UNIQUE" in str(e):
+                msg = "Error: Public Key is not UNIQUE"
+                return Response(msg, status=HTTPStatus.CONFLICT,
+                                mimetype='text/plain')
+            else:
+                msg = "Error: {}".format(e)
+                return Response(msg, status=HTTPStatus.BAD_REQUEST,
+                                mimetype='text/plain')
+    return render_template("export_onetime_conf.html", iface=iface)
